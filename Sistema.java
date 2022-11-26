20801880ce8fa6399ea707caf03fd801c3cfb74c
Dev-Hauffman <rauf.rodrigues@acad.pucrs.br>
2022-11-26 17:26:47 -0300

    Consertando acesso concorrent em lista "order"

--- Sistema.java
+++ Sistema.java
@@ -140,6 +140,7 @@ public class Sistema {
 		public void run() { 		// execucao da CPU supoe que o contexto da CPU, vide acima, esta devidamente setado	
 			while (true) {
 				try {
+					System.out.println("CPU says: I'm Waiting!");
 					semaCPU.acquire();
 					System.out.println("CPU says: I'm free!");
 				} catch (InterruptedException e) {
@@ -441,12 +442,15 @@ public class Sistema {
 						toBeSaved.state.registers = reg;
 						processManager.ready.remove(0);
 						processManager.blocked.add(toBeSaved);
+						vm.cpu.irpt = Interrupts.noInterrupt;
 						semaSch.release();
 						break;
 
 					case intIO:
+						System.out.println("blocked list size: " + processManager.blocked.size());
 						processManager.ready.add(processManager.blocked.get(0));
 						processManager.blocked.remove(0);
+						semaSch.release(); //is this needed?
 						break;
 				
 					default:
@@ -1031,18 +1035,21 @@ public class Sistema {
 
 		public void getInput(int position) {
 			order.add(new Order(position));
+			System.out.println("Order size:" + order.size());
 			shell.canInput = true;
-			
 		}
 
 		public void run(){
 			while(true){
-				if (order.size() > 0) {					
+				if (!List.copyOf(order).isEmpty()) {	
+					// System.out.println("There are orders!");
 					if (order.get(0).saveInMemoryValue != -1) {
+						System.out.println("Order says: I've got a value!");
 						vm.mem.m[order.get(0).memoryPosition].p = order.get(0).saveInMemoryValue;
 						if (vm.cpu.irpt == Interrupts.noInterrupt) {
 							vm.cpu.irpt = Interrupts.intIO;
 							order.remove(0);
+							semaCPU.release(); // is this necessary?
 						}
 					}
 				}
@@ -1102,7 +1109,7 @@ public class Sistema {
 	 		new Word(Opcode.STOP, -1, -1, -1),   // 9   	stop
 	 		new Word(Opcode.DATA, -1, -1, -1) }; // 10   ao final o valor do fatorial estará na posição 10 da memória                                    
 		
-	   public Word[] progMinimo = new Word[] {
+	    public Word[] progMinimo = new Word[] {
 		    new Word(Opcode.LDI, 0, -1, 999), 		
 			new Word(Opcode.STD, 0, -1, 10), 
 			new Word(Opcode.STD, 0, -1, 11), 
@@ -1118,9 +1125,9 @@ public class Sistema {
 			new Word(Opcode.DATA, -1, -1, -1),
 			new Word(Opcode.DATA, -1, -1, -1),
 			new Word(Opcode.DATA, -1, -1, -1),
-		 };
+		};
 
-	   public Word[] fibonacci10 = new Word[] { // mesmo que prog exemplo, so que usa r0 no lugar de r8
+	    public Word[] fibonacci10 = new Word[] { // mesmo que prog exemplo, so que usa r0 no lugar de r8
 			new Word(Opcode.LDI, 1, -1, 0), 
 			new Word(Opcode.STD, 1, -1, 20),   
 			new Word(Opcode.LDI, 2, -1, 1),
@@ -1173,7 +1180,7 @@ public class Sistema {
            new Word(Opcode.STOP, -1, -1, -1), // POS 17
            new Word(Opcode.DATA, -1, -1, -1)  };//POS 18	
 		   
-	       public Word[] fibonacciTRAP = new Word[] { // mesmo que prog exemplo, so que usa r0 no lugar de r8
+		public Word[] fibonacciTRAP = new Word[] { // mesmo que prog exemplo, so que usa r0 no lugar de r8
 			new Word(Opcode.LDI, 8, -1, 1),// leitura
 			new Word(Opcode.LDI, 9, -1, 100),//endereco a guardar
 			new Word(Opcode.TRAP, -1, -1, -1),
@@ -1229,90 +1236,143 @@ public class Sistema {
 			new Word(Opcode.DATA, -1, -1, -1),
 			new Word(Opcode.DATA, -1, -1, -1),
 			new Word(Opcode.DATA, -1, -1, -1),
+			new Word(Opcode.DATA, -1, -1, -1),
+			new Word(Opcode.DATA, -1, -1, -1),
+			new Word(Opcode.DATA, -1, -1, -1),
+			new Word(Opcode.DATA, -1, -1, -1),
+			new Word(Opcode.DATA, -1, -1, -1),
+			new Word(Opcode.DATA, -1, -1, -1),
+			new Word(Opcode.DATA, -1, -1, -1),
+			new Word(Opcode.DATA, -1, -1, -1),
+			new Word(Opcode.DATA, -1, -1, -1),
+			new Word(Opcode.DATA, -1, -1, -1),
+			new Word(Opcode.DATA, -1, -1, -1),
+			new Word(Opcode.DATA, -1, -1, -1),
+			new Word(Opcode.DATA, -1, -1, -1),
+			new Word(Opcode.DATA, -1, -1, -1),
+			new Word(Opcode.DATA, -1, -1, -1),
+			new Word(Opcode.DATA, -1, -1, -1),
+			new Word(Opcode.DATA, -1, -1, -1),
+			new Word(Opcode.DATA, -1, -1, -1),
+			new Word(Opcode.DATA, -1, -1, -1),
+			new Word(Opcode.DATA, -1, -1, -1),
+			new Word(Opcode.DATA, -1, -1, -1),
+			new Word(Opcode.DATA, -1, -1, -1),
+			new Word(Opcode.DATA, -1, -1, -1),
+			new Word(Opcode.DATA, -1, -1, -1),
+			new Word(Opcode.DATA, -1, -1, -1),
+			new Word(Opcode.DATA, -1, -1, -1),
+			new Word(Opcode.DATA, -1, -1, -1),
+			new Word(Opcode.DATA, -1, -1, -1),
+			new Word(Opcode.DATA, -1, -1, -1),
+			new Word(Opcode.DATA, -1, -1, -1),
+			new Word(Opcode.DATA, -1, -1, -1),
+			new Word(Opcode.DATA, -1, -1, -1),
+			new Word(Opcode.DATA, -1, -1, -1),
+			new Word(Opcode.DATA, -1, -1, -1),
+			new Word(Opcode.DATA, -1, -1, -1),
+			new Word(Opcode.DATA, -1, -1, -1),
+			new Word(Opcode.DATA, -1, -1, -1),
+			new Word(Opcode.DATA, -1, -1, -1),
+			new Word(Opcode.DATA, -1, -1, -1),
+			new Word(Opcode.DATA, -1, -1, -1),
+			new Word(Opcode.DATA, -1, -1, -1),
+			new Word(Opcode.DATA, -1, -1, -1),
+			new Word(Opcode.DATA, -1, -1, -1),
+			new Word(Opcode.DATA, -1, -1, -1),
+			new Word(Opcode.DATA, -1, -1, -1),
+			new Word(Opcode.DATA, -1, -1, -1),
+			new Word(Opcode.DATA, -1, -1, -1),
+			new Word(Opcode.DATA, -1, -1, -1),
+			new Word(Opcode.DATA, -1, -1, -1),
+			new Word(Opcode.DATA, -1, -1, -1),
+			new Word(Opcode.DATA, -1, -1, -1),
+			new Word(Opcode.DATA, -1, -1, -1),
 			new Word(Opcode.DATA, -1, -1, -1)
-	};
+		};
 
-	public Word[] PB = new Word[] {
-		//dado um inteiro em alguma posição de memória,
-		// se for negativo armazena -1 na saída; se for positivo responde o fatorial do número na saída
-		new Word(Opcode.LDI, 0, -1, 7),// numero para colocar na memoria
-		new Word(Opcode.STD, 0, -1, 50),
-		new Word(Opcode.LDD, 0, -1, 50),
-		new Word(Opcode.LDI, 1, -1, -1),
-		new Word(Opcode.LDI, 2, -1, 13),// SALVAR POS STOP
-		new Word(Opcode.JMPIL, 2, 0, -1),// caso negativo pula pro STD
-		new Word(Opcode.LDI, 1, -1, 1),
-		new Word(Opcode.LDI, 6, -1, 1),
-		new Word(Opcode.LDI, 7, -1, 13),
-		new Word(Opcode.JMPIE, 7, 0, 0), //POS 9 pula pra STD (Stop-1)
-		new Word(Opcode.MULT, 1, 0, -1),
-		new Word(Opcode.SUB, 0, 6, -1),
-		new Word(Opcode.JMP, -1, -1, 9),// pula para o JMPIE
-		new Word(Opcode.STD, 1, -1, 15),
-		new Word(Opcode.STOP, -1, -1, -1), // POS 14
-		new Word(Opcode.DATA, -1, -1, -1)}; //POS 15
+		public Word[] PB = new Word[] {
+			//dado um inteiro em alguma posição de memória,
+			// se for negativo armazena -1 na saída; se for positivo responde o fatorial do número na saída
+			new Word(Opcode.LDI, 0, -1, 7),// numero para colocar na memoria
+			new Word(Opcode.STD, 0, -1, 50),
+			new Word(Opcode.LDD, 0, -1, 50),
+			new Word(Opcode.LDI, 1, -1, -1),
+			new Word(Opcode.LDI, 2, -1, 13),// SALVAR POS STOP
+			new Word(Opcode.JMPIL, 2, 0, -1),// caso negativo pula pro STD
+			new Word(Opcode.LDI, 1, -1, 1),
+			new Word(Opcode.LDI, 6, -1, 1),
+			new Word(Opcode.LDI, 7, -1, 13),
+			new Word(Opcode.JMPIE, 7, 0, 0), //POS 9 pula pra STD (Stop-1)
+			new Word(Opcode.MULT, 1, 0, -1),
+			new Word(Opcode.SUB, 0, 6, -1),
+			new Word(Opcode.JMP, -1, -1, 9),// pula para o JMPIE
+			new Word(Opcode.STD, 1, -1, 15),
+			new Word(Opcode.STOP, -1, -1, -1), // POS 14
+			new Word(Opcode.DATA, -1, -1, -1)}; //POS 15
 
-public Word[] PC = new Word[] {
-		//Para um N definido (10 por exemplo)
-		//o programa ordena um vetor de N números em alguma posição de memória;
-		//ordena usando bubble sort
-		//loop ate que não swap nada
-		//passando pelos N valores
-		//faz swap de vizinhos se da esquerda maior que da direita
-		new Word(Opcode.LDI, 7, -1, 5),// TAMANHO DO BUBBLE SORT (N)
-		new Word(Opcode.LDI, 6, -1, 5),//aux N
-		new Word(Opcode.LDI, 5, -1, 46),//LOCAL DA MEMORIA
-		new Word(Opcode.LDI, 4, -1, 47),//aux local memoria
-		new Word(Opcode.LDI, 0, -1, 4),//colocando valores na memoria
-		new Word(Opcode.STD, 0, -1, 46),
-		new Word(Opcode.LDI, 0, -1, 3),
-		new Word(Opcode.STD, 0, -1, 47),
-		new Word(Opcode.LDI, 0, -1, 5),
-		new Word(Opcode.STD, 0, -1, 48),
-		new Word(Opcode.LDI, 0, -1, 1),
-		new Word(Opcode.STD, 0, -1, 49),
-		new Word(Opcode.LDI, 0, -1, 2),
-		new Word(Opcode.STD, 0, -1, 50),//colocando valores na memoria até aqui - POS 13
-		new Word(Opcode.LDI, 3, -1, 25),// Posicao para pulo CHAVE 1
-		new Word(Opcode.STD, 3, -1, 99),
-		new Word(Opcode.LDI, 3, -1, 22),// Posicao para pulo CHAVE 2
-		new Word(Opcode.STD, 3, -1, 98),
-		new Word(Opcode.LDI, 3, -1, 38),// Posicao para pulo CHAVE 3
-		new Word(Opcode.STD, 3, -1, 97),
-		new Word(Opcode.LDI, 3, -1, 25),// Posicao para pulo CHAVE 4 (não usada)
-		new Word(Opcode.STD, 3, -1, 96),
-		new Word(Opcode.LDI, 6, -1, 0),//r6 = r7 - 1 POS 22
-		new Word(Opcode.ADD, 6, 7, -1),
-		new Word(Opcode.SUBI, 6, -1, 1),//ate aqui
-		new Word(Opcode.JMPIEM, -1, 6, 97),//CHAVE 3 para pular quando r7 for 1 e r6 0 para interomper o loop de vez do programa
-		new Word(Opcode.LDX, 0, 5, -1),//r0 e r1 pegando valores das posições da memoria POS 26
-		new Word(Opcode.LDX, 1, 4, -1),
-		new Word(Opcode.LDI, 2, -1, 0),
-		new Word(Opcode.ADD, 2, 0, -1),
-		new Word(Opcode.SUB, 2, 1, -1),
-		new Word(Opcode.ADDI, 4, -1, 1),
-		new Word(Opcode.SUBI, 6, -1, 1),
-		new Word(Opcode.JMPILM, -1, 2, 99),//LOOP chave 1 caso neg procura prox
-		new Word(Opcode.STX, 5, 1, -1),
-		new Word(Opcode.SUBI, 4, -1, 1),
-		new Word(Opcode.STX, 4, 0, -1),
-		new Word(Opcode.ADDI, 4, -1, 1),
-		new Word(Opcode.JMPIGM, -1, 6, 99),//LOOP chave 1 POS 38
-		new Word(Opcode.ADDI, 5, -1, 1),
-		new Word(Opcode.SUBI, 7, -1, 1),
-		new Word(Opcode.LDI, 4, -1, 0),//r4 = r5 + 1 POS 41
-		new Word(Opcode.ADD, 4, 5, -1),
-		new Word(Opcode.ADDI, 4, -1, 1),//ate aqui
-		new Word(Opcode.JMPIGM, -1, 7, 98),//LOOP chave 2
-		new Word(Opcode.STOP, -1, -1, -1), // POS 45
-		new Word(Opcode.DATA, -1, -1, -1),
-		new Word(Opcode.DATA, -1, -1, -1),
-		new Word(Opcode.DATA, -1, -1, -1),
-		new Word(Opcode.DATA, -1, -1, -1),
-		new Word(Opcode.DATA, -1, -1, -1),
-		new Word(Opcode.DATA, -1, -1, -1),
-		new Word(Opcode.DATA, -1, -1, -1),
-		new Word(Opcode.DATA, -1, -1, -1)};
-   }
+		public Word[] PC = new Word[] {
+			//Para um N definido (10 por exemplo)
+			//o programa ordena um vetor de N números em alguma posição de memória;
+			//ordena usando bubble sort
+			//loop ate que não swap nada
+			//passando pelos N valores
+			//faz swap de vizinhos se da esquerda maior que da direita
+			new Word(Opcode.LDI, 7, -1, 5),// TAMANHO DO BUBBLE SORT (N)
+			new Word(Opcode.LDI, 6, -1, 5),//aux N
+			new Word(Opcode.LDI, 5, -1, 46),//LOCAL DA MEMORIA
+			new Word(Opcode.LDI, 4, -1, 47),//aux local memoria
+			new Word(Opcode.LDI, 0, -1, 4),//colocando valores na memoria
+			new Word(Opcode.STD, 0, -1, 46),
+			new Word(Opcode.LDI, 0, -1, 3),
+			new Word(Opcode.STD, 0, -1, 47),
+			new Word(Opcode.LDI, 0, -1, 5),
+			new Word(Opcode.STD, 0, -1, 48),
+			new Word(Opcode.LDI, 0, -1, 1),
+			new Word(Opcode.STD, 0, -1, 49),
+			new Word(Opcode.LDI, 0, -1, 2),
+			new Word(Opcode.STD, 0, -1, 50),//colocando valores na memoria até aqui - POS 13
+			new Word(Opcode.LDI, 3, -1, 25),// Posicao para pulo CHAVE 1
+			new Word(Opcode.STD, 3, -1, 99),
+			new Word(Opcode.LDI, 3, -1, 22),// Posicao para pulo CHAVE 2
+			new Word(Opcode.STD, 3, -1, 98),
+			new Word(Opcode.LDI, 3, -1, 38),// Posicao para pulo CHAVE 3
+			new Word(Opcode.STD, 3, -1, 97),
+			new Word(Opcode.LDI, 3, -1, 25),// Posicao para pulo CHAVE 4 (não usada)
+			new Word(Opcode.STD, 3, -1, 96),
+			new Word(Opcode.LDI, 6, -1, 0),//r6 = r7 - 1 POS 22
+			new Word(Opcode.ADD, 6, 7, -1),
+			new Word(Opcode.SUBI, 6, -1, 1),//ate aqui
+			new Word(Opcode.JMPIEM, -1, 6, 97),//CHAVE 3 para pular quando r7 for 1 e r6 0 para interomper o loop de vez do programa
+			new Word(Opcode.LDX, 0, 5, -1),//r0 e r1 pegando valores das posições da memoria POS 26
+			new Word(Opcode.LDX, 1, 4, -1),
+			new Word(Opcode.LDI, 2, -1, 0),
+			new Word(Opcode.ADD, 2, 0, -1),
+			new Word(Opcode.SUB, 2, 1, -1),
+			new Word(Opcode.ADDI, 4, -1, 1),
+			new Word(Opcode.SUBI, 6, -1, 1),
+			new Word(Opcode.JMPILM, -1, 2, 99),//LOOP chave 1 caso neg procura prox
+			new Word(Opcode.STX, 5, 1, -1),
+			new Word(Opcode.SUBI, 4, -1, 1),
+			new Word(Opcode.STX, 4, 0, -1),
+			new Word(Opcode.ADDI, 4, -1, 1),
+			new Word(Opcode.JMPIGM, -1, 6, 99),//LOOP chave 1 POS 38
+			new Word(Opcode.ADDI, 5, -1, 1),
+			new Word(Opcode.SUBI, 7, -1, 1),
+			new Word(Opcode.LDI, 4, -1, 0),//r4 = r5 + 1 POS 41
+			new Word(Opcode.ADD, 4, 5, -1),
+			new Word(Opcode.ADDI, 4, -1, 1),//ate aqui
+			new Word(Opcode.JMPIGM, -1, 7, 98),//LOOP chave 2
+			new Word(Opcode.STOP, -1, -1, -1), // POS 45
+			new Word(Opcode.DATA, -1, -1, -1),
+			new Word(Opcode.DATA, -1, -1, -1),
+			new Word(Opcode.DATA, -1, -1, -1),
+			new Word(Opcode.DATA, -1, -1, -1),
+			new Word(Opcode.DATA, -1, -1, -1),
+			new Word(Opcode.DATA, -1, -1, -1),
+			new Word(Opcode.DATA, -1, -1, -1),
+			new Word(Opcode.DATA, -1, -1, -1)
+		};
+    }
 }
 
